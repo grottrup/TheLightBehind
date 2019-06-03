f7140a2bbbd1c706db6bef5e521c254de59420dd
grottrup <grottrup@protonmail.com>
2019-06-03 16:22:55 +0200

    Update LightBehindTheDoor.rb

--- LightBehindTheDoor.rb
+++ LightBehindTheDoor.rb
@@ -1,83 +1,90 @@
 
 in_thread do
   live_loop :guilt do
-    use_synth :dark_ambience
-    sleep 0.5
-    play :C2, amp: 2, attack: 0.1, sustain: 0.2
-    sleep 0.75
-    play :G , attack: 0.1, amp: 2
-    sleep 0.25
-    play :A , attack: 0.1, amp: 4
-    sleep 0.5
-    play :B , attack: 0.1, amp: 2
-    sleep 0.5
-    play :A , attack: 0.1, amp: 4
-    sleep 0.25
-    play :G, attack: 0.1, amp: 2
-    sleep 0.25
-    sample :bd_808
-    play :Fs, attack: 0.1, amp: 4
-    sleep 0.50
-    with_fx :echo, mix: 1, phase: 0.5 do
-      play :G, amp: 2, attack: 0.3, sustain: 0.2, release: 0.8, sustain_level: 0.6
+    2.times do
+      use_synth :dark_ambience
+      sleep 0.5
+      play :C2, amp: 2, attack: 0.1, sustain: 0.2
+      sleep 0.75
+      play :G , attack: 0.1, amp: 2
+      sleep 0.25
+      play :A , attack: 0.1, amp: 4
+      sleep 0.5
+      play :B , attack: 0.1, amp: 2
+      sleep 0.5
+      play :A , attack: 0.1, amp: 4
+      sleep 0.25
+      play :G, attack: 0.1, amp: 2
+      sleep 0.25
+      sample :bd_808
+      play :Fs, attack: 0.1, amp: 4
+      sleep 0.50
+      with_fx :echo, mix: 1, phase: 0.5 do
+        play :G, amp: 2, attack: 0.3, sustain: 0.2, release: 0.8, sustain_level: 0.6
+      end
+      sleep 0.75
+      
+      sample :drum_heavy_kick, amp: 0.5
+      sample :ambi_drone
+      
+      use_synth :growl
+      play :A , attack: 0.1, amp: 1
+      sleep 0.25
+      play :B , attack: 0.1 , amp: 0.5
+      sleep 0.5
+      play :A , attack: 0.1 , amp: 1
+      sleep 0.5
+      play :G, amp: 1, attack: 0.3, sustain: 0.2, release: 0.8, sustain_level: 0.6
+      sleep 0.25
+      play :Fs, attack: 0.1, amp: 0.75
+      sleep 0.25
+      play :G, amp: 1, attack: 0.3, sustain: 0.2, release: 0.8, sustain_level: 0.6
+      sleep 1
+      
+      use_synth :dark_ambience
+      play :Fs, attack: 0.1
+      sleep 0.5
+      play :D, attack: 0.1
+      sleep 0.5
+      use_synth :growl
+      play :C2, attack: 0.1
+      sleep 0.5
+      use_synth :dark_ambience
+      play :D, attack: 0.1
+      sleep 0.5
+      use_synth :growl
+      play :C2, attack: 0.1
+      sleep 0.75
+      
+      use_synth :pluck
+      play :E
+      sleep 0.25
+      play :G
+      sleep 1
+      play :G
+      sleep 0.5
+      play :A
+      sleep 0.5
+      play :B
+      sleep 0.25
+      play :C
+      sleep 0.75
+      
+      use_synth :dark_ambience
+      play :G, amp: 4
+      sleep 0.5
+      play :A, amp: 4
+      sleep 0.75
+      play :C, amp: 4
+      sleep 0.25
+      play :D, amp: 4
+      sleep 1
     end
-    sleep 0.75
-    
-    sample :drum_heavy_kick, amp: 0.5
-    sample :ambi_drone
-    
-    use_synth :growl
-    play :A , attack: 0.1, amp: 1
-    sleep 0.25
-    play :B , attack: 0.1 , amp: 0.5
-    sleep 0.5
-    play :A , attack: 0.1 , amp: 1
-    sleep 0.5
-    play :G, amp: 1, attack: 0.3, sustain: 0.2, release: 0.8, sustain_level: 0.6
-    sleep 0.25
-    play :Fs, attack: 0.1, amp: 0.75
-    sleep 0.25
-    play :G, amp: 1, attack: 0.3, sustain: 0.2, release: 0.8, sustain_level: 0.6
-    sleep 1
-    
-    use_synth :dark_ambience
-    play :Fs, attack: 0.1
-    sleep 0.5
-    play :D, attack: 0.1
-    sleep 0.5
-    use_synth :growl
-    play :C2, attack: 0.1
-    sleep 0.5
-    use_synth :dark_ambience
-    play :D, attack: 0.1
-    sleep 0.5
-    use_synth :growl
-    play :C2, attack: 0.1
-    sleep 0.75
-    
     use_synth :pluck
-    play :E
-    sleep 0.25
-    play :G
-    sleep 1
-    play :G
-    sleep 0.5
-    play :A
-    sleep 0.5
-    play :B
-    sleep 0.25
-    play :C
-    sleep 0.75
-    
-    use_synth :dark_ambience
-    play :G, amp: 4
-    sleep 0.5
-    play :A, amp: 4
-    sleep 0.75
-    play :C, amp: 4
-    sleep 0.25
-    play :D, amp: 4
-    sleep 1
+    with_fx :echo, mix: 0.5, phase: 0.5 do
+      play :G, amp: 4, attack: 0.3, sustain: 1.5, release: 4, sustain_level: 0.6
+    end
+    sleep 4
   end
 end
 
