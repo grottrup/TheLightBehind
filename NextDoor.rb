f95a91927df41232a4c4f54464ceb37d9e2d1c1a
grottrup <41370210+grottrup@users.noreply.github.com>
2019-06-21 21:48:56 +0200

    Update NextDoor.rb

--- NextDoor.rb
+++ NextDoor.rb
@@ -1,16 +1,16 @@
 live_loop :guilt do
   loop do
-    use_synth :hollow
+    use_synth :sound_in_stereo
     with_fx :echo, mix: 0.5, phase: 0.5 do
-      play :B, amp: 1, attack: 0.3, sustain: 0.4, release: 0.5, sustain_level: 0.6
+      play :B, amp: 0.5, attack: 0.3, sustain: 0.4, release: 0.5, sustain_level: 0.6
     end
     sleep 0.5
     with_fx :echo, mix: 0.5, phase: 0.5 do
-      play :A, amp: 2, attack: 0.3, sustain: 1, release: 0.9, sustain_level: 0.6
+      play :A, amp: 1, attack: 0.3, sustain: 1, release: 0.9, sustain_level: 0.6
     end
     sleep 1
     with_fx :echo, mix: 0.5, phase: 0.5 do
-      play :G, amp: 3, attack: 0.3, sustain: 1, release: 4, sustain_level: 0.6
+      play :G, amp: 1, attack: 0.3, sustain: 1, release: 4, sustain_level: 0.6
     end
     sleep 4
   end
@@ -18,10 +18,11 @@ live_loop :guilt do
 end
 
 in_thread do
+  sleep 5
   loop do
     sample :bass_trance_c
     sleep 1.25
-    sample :glitch_bass_g
+    sample :bd_808
     sleep 0.75
     sample :bass_hit_c
     sleep 0.5
@@ -31,18 +32,19 @@ in_thread do
 end
 
 in_thread do
+  sleep 2.5
   loop do
     2.times do
-      sample :guit_harmonics
-      sleep 2
+      sample :bd_ada
+      sleep 10
     end
-    sample :guit_e_fifths
-    sleep 1
+    sample :drum_bass_soft
+    sleep 4
   end
 end
 
 in_thread do
-  sleep 0.5
+  sleep 1.5
   loop do
     sample :drum_heavy_kick
     sleep 1
